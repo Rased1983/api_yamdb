481f851441b250637f7e4fb544863a0e528d91e0
AkuLinker <AkuLinker@gmail.com>
2021-11-12 12:23:30 +0300

    little change

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,13 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (UserViewSet, GetTokenView,
-                       EmailAndNewUserRegistrationView, GenreViewSet, CategoryViewSet)
+                       EmailAndNewUserRegistrationView,
+                       GenreViewSet, CategoryViewSet)
 
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='api_users')
 router.register(r'genres', GenreViewSet, basename='api_genres')
-router.register(r'categories', GenreViewSet, basename='api_categories')
+router.register(r'categories', CategoryViewSet, basename='api_categories')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
