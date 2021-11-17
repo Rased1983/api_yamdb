f3a70643fd1e25493fae7b4e12ef31bdfa5aa109
AkuLinker <Akulinker@gmail.com>
2021-11-17 15:34:26 +0300

    refactoring

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -35,7 +35,7 @@ class User(AbstractUser):
         verbose_name='Роль пользователя',
     )
     confirmation_code = models.CharField(
-        max_length=20,
+        max_length=40,
         null=True,
         blank=True,
         verbose_name='Код подтверждения',
